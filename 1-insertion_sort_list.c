9c95654619464ba5ce19ec2b0f2046b5f4c4e3e5
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-10-20 09:56:45 -0700

    tasks .0 - .1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list -  Insertion sort algorithm
+ *
+ * @list: pointer to the header of doubly linked list
+ * Return: void
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp, *current;
+
+	if (!*list || !(*list)->next)
+		return;
+
+	current = *list;
+
+	while (current)
+	{
+		while (current->next && (current->next->n < current->n))
+		{
+			temp = current->next;
+			current->next = temp->next;
+			temp->prev = current->prev;
+
+			if (current->prev)
+				current->prev->next = temp;
+
+			if (temp->next)
+				temp->next->prev = current;
+
+			current->prev = temp;
+			temp->next = current;
+
+			if (temp->prev)
+				current = temp->prev;
+			else
+				*list = temp;
+
+			print_list(*list);
+		}
+		current = current->next;
+	}
+
+}
